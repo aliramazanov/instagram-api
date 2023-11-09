06b88c82e9afbccde9c9213b64793f3913052ee5
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-11-09 15:09:57 +0400

    get and delete added

--- src/routes/newsroute.ts
+++ src/routes/newsroute.ts
@@ -2,6 +2,16 @@ import { Router, Request, Response } from "express";
 import { User } from "../models/userModel";
 const instagramRouter = Router();
 
+instagramRouter.get("/api", async (req: Request, res: Response) => {
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "An unknown error occurred." });
+  }
+});
+
 instagramRouter.post("/api", async (req: Request, res: Response) => {
   try {
     const newUser = {
@@ -12,6 +22,20 @@ instagramRouter.post("/api", async (req: Request, res: Response) => {
     };
     const user = await User.create(newUser);
     return res.status(201).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "An unknown error occurred." });
+  }
+});
+
+instagramRouter.delete("/api/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await User.findByIdAndDelete(id);
+    if (!result) {
+      return res.status(404).json({ message: "User has not been found" });
+    }
+    return res.status(200).send({ message: "User has been deleted" });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "An unknown error occurred." });
