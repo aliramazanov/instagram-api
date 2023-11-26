ed0474c6d943dc7708eb7302fd5c92b6861c2814
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-26 11:26:31 +0400

    remove: cors package

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import cors from "cors";
-import { registerUser, loginUser } from "../controllers/authController";
+import { loginUser, registerUser } from "../controllers/authController";
 
 const authRoute = express.Router();
-authRoute.use(cors());
 
 authRoute.post("/auth/register", registerUser);
 authRoute.post("/auth/login", loginUser);
