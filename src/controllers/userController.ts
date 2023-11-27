ca486f26d92a95dc6e7c23e9b45226207a93c51f
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-27 10:17:30 +0400

    update: delete user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -267,10 +267,25 @@ export const updatePassword = async (req: Request, res: Response) => {
 
 export const deleteUser = async (req: Request, res: Response) => {
   try {
-    const userId = (req.user as any)?.id;
+    let token = "";
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } else {
+      return res.status(401).json({ message: "User not authenticated" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY || "") as {
+      id: string;
+    };
+
+    const userId = decodedToken.id;
 
     if (!userId) {
-      return res.status(401).send({ message: "User not authenticated" });
+      return res.status(401).json({ message: "User not authenticated" });
     }
 
     const user = await User.findById(userId);
@@ -285,9 +300,9 @@ export const deleteUser = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    return res.status(200).send({ message: "User has been deleted" });
+    return res.status(200).json({ message: "User has been deleted" });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ message: "An unknown error occurred." });
+    return res.status(500).json({ message: "An unknown error occurred." });
   }
 };
