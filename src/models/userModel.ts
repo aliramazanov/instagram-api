162f823b8d00c6ef571e7c9a2c37a98e66c860ac
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-04 20:45:18 +0400

    update: user schema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: false,
     unique: true,
     min: 4,
     max: 20,
