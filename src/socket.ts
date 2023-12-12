68492660f991dd49360d4085c7894c0771cf4ac5
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-12 19:00:44 +0400

    update: cors on socket

--- src/socket.ts
+++ src/socket.ts
@@ -7,6 +7,7 @@ export const startSocket = (server: HttpServer) => {
       origin: [
         "http://localhost:5173",
         "https://instagram-client-abb.vercel.app",
+        "https://instagram-client-zvbb.onrender.com/",
       ],
       methods: ["GET", "POST"],
     },
@@ -16,33 +17,35 @@ export const startSocket = (server: HttpServer) => {
     next();
   });
 
-  io.on("connection", (socket: Socket & { handshake?: { query?: { userId?: string } } }) => {
-    const userId: any = socket.handshake?.query?.userId;
-    socket.join(userId);
-  
-    console.log(`User ${userId} connected`);
-  
-    socket.emit("socketId", userId);
-    io.to(socket.id).emit("user_connected", userId);
-  
-    socket.onAny((eventName, ...args) => {
-      console.log(`Received message from ${userId}: ${eventName}`, args);
-    });
-  
-    socket.on("private_message", (data: { to: string; message: string }) => {
-      const { to, message } = data;
-      if (io.sockets.adapter.rooms.has(to)) {
-        io.to(to).emit("private_message", { from: userId, message });
-      } else {
-        console.error(`Recipient ${to} is not connected.`);
-      }
-    });
-  
-    socket.on("disconnect", () => {
-      console.log(`User disconnected: ${userId}`);
-    });
-  });
-  
+  io.on(
+    "connection",
+    (socket: Socket & { handshake?: { query?: { userId?: string } } }) => {
+      const userId: any = socket.handshake?.query?.userId;
+      socket.join(userId);
+
+      console.log(`User ${userId} connected`);
+
+      socket.emit("socketId", userId);
+      io.to(socket.id).emit("user_connected", userId);
+
+      socket.onAny((eventName, ...args) => {
+        console.log(`Received message from ${userId}: ${eventName}`, args);
+      });
+
+      socket.on("private_message", (data: { to: string; message: string }) => {
+        const { to, message } = data;
+        if (io.sockets.adapter.rooms.has(to)) {
+          io.to(to).emit("private_message", { from: userId, message });
+        } else {
+          console.error(`Recipient ${to} is not connected.`);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log(`User disconnected: ${userId}`);
+      });
+    }
+  );
 
   return io;
 };
